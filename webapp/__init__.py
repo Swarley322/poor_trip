83615ed3c2cad53a26c31d5af953777280e484cb
Dmitry Kim <kim.dima.93@gmail.com>
2020-03-11 20:30:57 +0300

    index

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
 from webapp.get_all_hotels import get_best_hotels
 # from webapp.skyscanner import get_tickets
 from webapp.model import db
+from webapp.forms import LoginForm
 
 
 def create_app():  # export FLASK_APP=webapp && export FLASK_ENV=development && flask run
@@ -9,13 +10,17 @@ def create_app():  # export FLASK_APP=webapp && export FLASK_ENV=development && 
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    @app.route('/')
+    @app.route('/', methods=["GET", "POST"])
+    def start():
+        form = LoginForm()
+        if form.validate_on_submit():
+            city = request.form["city"]
+            return redirect(url_for("index", city=city))
+        return render_template('start.html', form=form)
+
+    @app.route('/index')
     def index():
-        hotel_list = get_best_hotels('Токио')
-        # tickets_list = get_tickets("Moscow", "Rome")
-        # return render_template('index.html',
-        #                        hotel_list=hotel_list,
-        #                        tickets_list=tickets_list)
+        hotel_list = get_best_hotels(request.args.get("city"))
         return render_template('index.html', hotel_list=hotel_list)
 
     return app
