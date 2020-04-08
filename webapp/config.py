182595fa11046a31fdc849abea33f24330c30089
Dmitry Kim <kim.dima.93@gmail.com>
2020-04-09 00:20:46 +0300

    fix

--- webapp/config.py
+++ webapp/config.py
@@ -1,5 +1,4 @@
 import os
-from celery.schedules import crontab
 from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -18,31 +17,3 @@ CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 # CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
 # CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
-# SCHEDULE = os.environ.get("SCHEDULE")
-
-# CELERY_TASK_SERIALIZER = 'json'
-
-# CELERYBEAT_SCHEDULE = {
-#         "parsing": {
-#             "task": "webapp.tasks.get_hotels",
-#             "schedule": crontab(minute="*/6"),
-#             "args": ()
-#         },
-#         "create_city_list": {
-#             "task": "webapp.tasks.create_city_list",
-#             "schedule": crontab(minute=0, hour=0),
-#             "args": ()
-#         },
-#         "get_live_proces": {
-#             "task": "webapp.tasks.get_live_prices",
-#             "schedule": crontab(minute=0, hour=1),
-#             "args": ()
-#         }
-}
-# CELERYBEAT_SCHEDULE = {
-#         "tasker": {
-#             "task": "webapp.tasks.task1",
-#             "schedule": 5.0,
-#             "args": ()
-#         }
-# }
